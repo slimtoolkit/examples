15bb4967acbba8521551d8db3683701f62a9bdbf
Kyle Quest <kcq.public@gmail.com>
2020-05-23 00:04:16 -0700

    ubuntu.com example fixes and linting/xray helper scripts

--- 3rdparty/ubuntu-com/run_fat.sh
+++ 3rdparty/ubuntu-com/run_fat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --rm -p 8222:80 ubuntu-com
+docker run -it --rm -p 8222:80 --env SECRET_KEY=insecure_secret_key --env DATABASE_URL=sqlite:///db.sqlite3 ubuntu-com
